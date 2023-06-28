2bc291a0c12dd78dcc3d7619f43a523d8df00de5
rehadhasan <rehadhasan664@gmail.com>
2023-06-28 12:16:10 +0600

    update app.js section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { Toaster } from "react-hot-toast";
 import "./App.css";
+import Routes from "./Router/Routes";
 
 function App() {
-  return <div>Hotel Booking</div>;
+  return (
+    <div className="">
+      <Routes></Routes>
+      <Toaster
+        toastOptions={{
+          className: "mt-12 mr-6",
+          style: {
+            border: "1px solid #713200",
+            padding: "24px",
+          },
+        }}
+        duration="4000"
+        position="top-center"
+        reverseOrder={false}
+      ></Toaster>
+    </div>
+  );
 }
 
 export default App;
