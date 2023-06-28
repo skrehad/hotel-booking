f65276c2da5045ea64d8faf800e70e277823f005
rehadhasan <rehadhasan664@gmail.com>
2023-06-28 12:24:48 +0600

    added About section

--- src/Router/Routes.js
+++ src/Router/Routes.js
@@ -4,14 +4,14 @@ import About from "../Components/About/About";
 import Blogs from "../Components/Blogs/Blogs";
 import Contact from "../Components/Contact/Contact";
 import Home from "../Components/Home/Home";
-import Login from "../Components/Login/Login";
+// import Login from "../Components/Login/Login";
 import NotFound from "../Components/NotFound/NotFound";
-import Register from "../Components/Register/Register";
-import MyOrder from "../Components/MyOrder/MyOrder";
+// import Register from "../Components/Register/Register";
+// import MyOrder from "../Components/MyOrder/MyOrder";
 import Products from "../Components/Products/Products";
-import DetailsProduct from "../Components/DetailsProduct/DetailsProduct";
-import PrivateRouter from "./PrivateRouter";
-import OverAllReview from "../Components/OverAllReview/OverAllReview";
+// import DetailsProduct from "../Components/DetailsProduct/DetailsProduct";
+// import PrivateRouter from "./PrivateRouter";
+// import OverAllReview from "../Components/OverAllReview/OverAllReview";
 import Main from "../Layouts/Main";
 
 const Routes = () => {
@@ -32,35 +32,35 @@ const Routes = () => {
           path: "/products",
           element: <Products></Products>,
         },
-        {
-          path: "/products/:id",
-          loader: async ({ params }) => {
-            return fetch(
-              `https://easy-shop-backend-server.vercel.app/products/${params.id}`
-            );
-          },
-          element: (
-            <PrivateRouter>
-              <DetailsProduct></DetailsProduct>
-            </PrivateRouter>
-          ),
-        },
-        {
-          path: "/myOrders",
-          element: (
-            <PrivateRouter>
-              <MyOrder></MyOrder>
-            </PrivateRouter>
-          ),
-        },
-        {
-          path: "/addReview",
-          element: (
-            <PrivateRouter>
-              <OverAllReview></OverAllReview>
-            </PrivateRouter>
-          ),
-        },
+        // {
+        //   path: "/products/:id",
+        //   loader: async ({ params }) => {
+        //     return fetch(
+        //       `https://easy-shop-backend-server.vercel.app/products/${params.id}`
+        //     );
+        //   },
+        //   element: (
+        //     <PrivateRouter>
+        //       <DetailsProduct></DetailsProduct>
+        //     </PrivateRouter>
+        //   ),
+        // },
+        // {
+        //   path: "/myOrders",
+        //   element: (
+        //     <PrivateRouter>
+        //       <MyOrder></MyOrder>
+        //     </PrivateRouter>
+        //   ),
+        // },
+        // {
+        //   path: "/addReview",
+        //   element: (
+        //     <PrivateRouter>
+        //       <OverAllReview></OverAllReview>
+        //     </PrivateRouter>
+        //   ),
+        // },
 
         {
           path: "/blog",
