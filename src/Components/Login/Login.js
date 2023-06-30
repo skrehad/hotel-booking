403d0369cf020a684ed73156387ecbb7b31a8132
rehadhasan <rehadhasan664@gmail.com>
2023-06-30 19:16:31 +0600

    update login section

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,32 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { BsGoogle, BsGithub } from "react-icons/bs";
+import { BsGoogle, BsFacebook } from "react-icons/bs";
+import "./Login.css";
 
 const Login = () => {
   return (
     <div>
-      <div className="card rounded-none bg-[url('https://i.ibb.co/dkGZY8x/St-Regis-Mauritius-Resort.jpg')] bg-cover shadow-2xl">
-        <div className="flex mx-auto my-14">
+      <div className="card rounded-none bg-[url('https://i.ibb.co/dkGZY8x/St-Regis-Mauritius-Resort.jpg')]  shadow-2xl">
+        <div className="flex mx-auto mt-14 mb-8">
           <img
             className="w-[50px] rounded-lg headerImage h-[50px]"
             src="https://i.ibb.co/NNT3GBS/favicon.jpg"
             alt=""
           />
-          <div className="ml-2 uppercase text-black italic font-bold mt-2.5 headerIcon">
+          <div className=" ml-2 uppercase text-black italic font-bold mt-3">
             Hotel Booking.com
           </div>
         </div>
 
-        <div className="card m-auto  rounded-none flex-shrink-0 w-full max-w-sm shadow-2xl">
+        <div className="card cardDiv m-auto flex-shrink-0 lg:w-full max-w-sm mb-20 shadow-3xl">
           <div>
-            <h1 className="text-3xl text-white text-center">Log In</h1>
+            <h1 className="text-3xl mt-10 text-white  text-center">Log In</h1>
           </div>
 
-          <div className="card-body">
+          <div className="card-body cardBody gap-0 px-16">
             <div className="form-control">
               <label className="label">
-                <span className="label-text text-white">Email</span>
+                <span className="label-text text-[18px] text-white">Email</span>
               </label>
+
               <input
                 type="text"
                 placeholder="Enter your Email"
@@ -35,7 +37,9 @@ const Login = () => {
             </div>
             <div className="form-control">
               <label className="label">
-                <span className="label-text text-white">Password</span>
+                <span className="label-text text-[18px] text-white">
+                  Password
+                </span>
               </label>
               <input
                 type="text"
@@ -43,7 +47,7 @@ const Login = () => {
                 className="input input-bordered bg-none"
               />
               <Link>
-                <span className="text-white hover:underline">
+                <span className="text-white hover:underline text-[12px]">
                   Forget Password
                 </span>
               </Link>
@@ -51,17 +55,28 @@ const Login = () => {
             <div className="form-control mt-6">
               <button className="btn btn-primary">Login</button>
             </div>
-            <Link className="text-white hover:underline" to="/signup">
-              New User! Please SignUp First...
-            </Link>
           </div>
-          <div className=" mx-auto mb-8 ">
-            <p className="underline text-center font-mono font-bold">OR</p>
+          <div className=" mx-auto mb-3 ">
+            <p className="underline mb-4 text-center text-white text-xl font-mono font-bold">
+              OR
+            </p>
             <div className="flex gap-5 mt-2">
-              <BsGoogle className="cursor-pointer text-black text-2xl"></BsGoogle>
-              <BsGithub className="cursor-pointer text-black text-2xl"></BsGithub>
+              <div className="flex btn btn-primary">
+                <BsFacebook className="cursor-pointer text-white text-2xl"></BsFacebook>
+                <p>Facebook</p>
+              </div>
+              <div className="flex btn btn-primary">
+                <BsGoogle className="cursor-pointer text-white text-2xl"></BsGoogle>
+                <p className="mr-4">Google</p>
+              </div>
             </div>
           </div>
+          <Link
+            className="text-white text-center mb-8 hover:underline"
+            to="/signup"
+          >
+            New User! Please SignUp First...
+          </Link>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        "3xl": "0 35px 60px 50px rgba(0, 0, 0, 0.3)",
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
