30a4f0f11f397ed9a121e52095741c41631d7714
rehadhasan <rehadhasan664@gmail.com>
2023-07-01 11:19:28 +0600

    update footer section

--- src/Shared/Footer/Footer.js
+++ src/Shared/Footer/Footer.js
@@ -4,45 +4,42 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <footer className="footer p-10 bg-base-200 text-base-content">
-      <div className="gap-0">
+      <div className="gap-0 m-auto">
         <img
-          className="w-[80px]"
+          className="w-[80px] m-auto"
           src="https://i.ibb.co/1MZDM9G/favicon.png"
           alt=""
           srcset=""
         />
-        <p>
-          Hotel Booking.com
-          <br />
-          Providing reliable hotels since 1992
-        </p>
+        <p className="m-auto">Hotel Booking.com</p>
+        <p className="m-auto">Providing reliable hotels since 1992</p>
       </div>
-      <div>
-        <span className="footer-title">Services</span>
-        <Link className="link link-hover">Branding</Link>
-        <Link className="link link-hover">Design</Link>
-        <Link className="link link-hover">Marketing</Link>
-        <Link className="link link-hover">Advertisement</Link>
+      <div className="m-auto">
+        <span className="footer-title m-auto">Services</span>
+        <Link className="link link-hover m-auto">Branding</Link>
+        <Link className="link link-hover m-auto">Design</Link>
+        <Link className="link link-hover m-auto">Marketing</Link>
+        <Link className="link link-hover m-auto">Advertisement</Link>
       </div>
-      <div>
-        <span className="footer-title">Company</span>
-        <Link to="/about" className="link link-hover">
+      <div className="m-auto">
+        <span className="footer-title m-auto">Company</span>
+        <Link to="/about" className="link m-auto link-hover">
           About us
         </Link>
-        <Link to="/contact" className="link link-hover">
+        <Link to="/contact" className="link m-auto link-hover">
           Contact
         </Link>
-        <Link to="/blog" className="link link-hover">
+        <Link to="/blog" className="link m-auto link-hover">
           Blog
         </Link>
-        <Link className="link link-hover">Press kit</Link>
+        <Link className="link m-auto link-hover">Press kit</Link>
       </div>
-      <div>
-        <span className="footer-title">Legal</span>
-        <Link className="link link-hover">Terms of use</Link>
-        <Link className="link link-hover">Privacy policy</Link>
-        <Link className="link link-hover">Cookie policy</Link>
-        <Link to="/login" className="link link-hover">
+      <div className="m-auto">
+        <span className="footer-title m-auto">Legal</span>
+        <Link className="link link-hover m-auto">Terms of use</Link>
+        <Link className="link link-hover m-auto">Privacy policy</Link>
+        <Link className="link link-hover m-auto">Cookie policy</Link>
+        <Link to="/login" className="link m-auto link-hover">
           Login
         </Link>
       </div>
