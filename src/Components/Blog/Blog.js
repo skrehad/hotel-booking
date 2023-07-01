04d75ff40120d815285c400a2666ed7faa5c0fe7
rehadhasan <rehadhasan664@gmail.com>
2023-07-02 03:29:00 +0600

    complete blog section

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -1,6 +1,14 @@
 import React from "react";
+import { useQuery } from "react-query";
+import Loading from "../../Shared/Loading/Loading";
 
 const Blog = () => {
+  const { isLoading, data: blogs } = useQuery("blogData", () =>
+    fetch("http://localhost:5000/blog").then((res) => res.json())
+  );
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
   return (
     <div>
       <div className="bg-[url('https://i.ibb.co/yswPhR5/photo-1445019980597-93fa8acb246c.jpg')] h-[200px] md:h-[300px] lg:h-[300px] bg-cover bg-no-repeat">
@@ -18,7 +26,53 @@ const Blog = () => {
         </div>
         <div className="lg:flex">
           <div className="w-full ">
-            <div class="max-w-sm mb-5 w-full md:mx-auto lg:max-w-full lg:flex">
+            {blogs
+              .concat()
+              .reverse()
+              .map((blog) => (
+                <div
+                  key={blog._id}
+                  class="max-w-sm mb-5 w-full md:mx-auto lg:max-w-full lg:flex"
+                >
+                  <div
+                    className="lg:h-auto lg:w-[280px] md:h-[250px] flex-none bg-cover text-center overflow-hidden"
+                    title=""
+                  >
+                    <img className="h-full" src={blog.image} alt="" srcset="" />
+                  </div>
+                  <div class="  bg-white     rounded-b p-4 flex flex-col justify-between leading-normal">
+                    <div class="mb-8">
+                      <div class="text-gray-900 font-bold text-xl mb-2">
+                        {blog.title}
+                      </div>
+                      <p class="text-black text-base">{blog.description}</p>
+                    </div>
+                    <div class="flex items-center mx-auto lg:mx-0 md:mx-0">
+                      {blog.publisherPhoto ? (
+                        <img
+                          class="w-10 h-10 rounded-full mr-4"
+                          src={blog.publisherPhoto}
+                          alt=""
+                        />
+                      ) : (
+                        <img
+                          alt=""
+                          class="w-10 h-10 rounded-full mr-4"
+                          src="https://i.ibb.co/S5PRg6x/download.jpg"
+                        />
+                      )}
+                      <div class="text-sm">
+                        <p class="text-black font-bold leading-none">
+                          {blog.publisherName}
+                        </p>
+                        <p class="text-gray-600">{blog.date}</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+
+            {/* <div class="max-w-sm mb-5 w-full md:mx-auto lg:max-w-full lg:flex">
               <div
                 className="h-48 bg-[url('https://i.ibb.co/HXZRg7d/baldwin-home12621-610x610.jpg')] lg:h-auto lg:w-60 md:h-[250px] flex-none bg-cover text-center overflow-hidden"
                 title=""
@@ -37,15 +91,7 @@ const Blog = () => {
                   </p>
                 </div>
                 <div class="flex items-center">
-                  {/* <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-              <div className="w-10 rounded-full">
-                {user && user.photoURL ? (
-                  <img src={user.photoURL} alt="" />
-                ) : (
-                  <img src="https://i.ibb.co/S5PRg6x/download.jpg" alt="" />
-                )}
-              </div>
-            </label> */}
+                  
 
                   <img
                     alt=""
@@ -58,38 +104,7 @@ const Blog = () => {
                   </div>
                 </div>
               </div>
-            </div>
-            <div class="max-w-sm mb-5 w-full md:mx-auto lg:max-w-full lg:flex">
-              <div
-                className="h-48 bg-[url('https://i.ibb.co/HXZRg7d/baldwin-home12621-610x610.jpg')] lg:h-auto lg:w-60 md:h-[250px] flex-none bg-cover text-center overflow-hidden"
-                title=""
-              ></div>
-              <div class="  bg-white     rounded-b p-4 flex flex-col justify-between leading-normal">
-                <div class="mb-8">
-                  <div class="text-gray-900 font-bold text-xl mb-2">
-                    Disclosing the Secrets of Success in Luxury Hospitality
-                  </div>
-                  <p class="text-gray-700 text-base">
-                    Every good day starts off with a cappuccino, and there’s no
-                    place better to enjoy some frothy caffeine than at the
-                    Bulgari Hotel. Meh synth Schlitz, tempor duis single-origin
-                    coffee ea next level ethnic fingerstache fanny pack nostrud.
-                    Photo booth...
-                  </p>
-                </div>
-                <div class="flex items-center">
-                  <img
-                    alt=""
-                    class="w-10 h-10 rounded-full mr-4"
-                    src="/img/jonathan.jpg"
-                  />
-                  <div class="text-sm">
-                    <p class="text-gray-900 leading-none">Jonathan Reinink</p>
-                    <p class="text-gray-600">Aug 18</p>
-                  </div>
-                </div>
-              </div>
-            </div>
+            </div> */}
           </div>
           <div className="w-none ">
             <div class="flex justify-center sticky top-20 flex-wrap ">
--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import AuthProvider from "./Context/AuthProvider/AuthProvider";
+import { QueryClient, QueryClientProvider } from "react-query";
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </AuthProvider>
   </React.StrictMode>
 );
