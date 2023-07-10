b83feaf67ba5c9582cf72ccaab95e0070101c619
rehadhasan <rehadhasan664@gmail.com>
2023-07-10 16:23:11 +0600

    added HomeFooter section in home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import HomeFooter from "./HomeFooter/HomeFooter";
 
 const Home = () => {
   return (
     <div>
       <div>home</div>
+      <HomeFooter></HomeFooter>
     </div>
   );
 };
