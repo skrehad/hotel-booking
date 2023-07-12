af571664d18dd04b46c09b360ac45ab44c2c74a7
rehadhasan <rehadhasan664@gmail.com>
2023-07-12 11:46:17 +0600

    update servicesIcont section in home components

--- src/Components/Home/ServicesIcon/ServicesIcon.js
+++ src/Components/Home/ServicesIcon/ServicesIcon.js
@@ -2,61 +2,50 @@ import React from "react";
 
 const ServicesIcon = () => {
   return (
-    <div>
-      <div className="grid gap-20  lg:grid-cols-5 md:grid-cols-5">
-        <div className="bg-white">
-          <img
-            className="mx-auto"
-            src="https://i.ibb.co/K07jKtz/icon-4-png.webp"
-            alt=""
-            srcset=""
-          />
-          <p className="text-xl text-center mt-4 font-serif text-black">
-            Transportation
-          </p>
-        </div>
-        <div>
-          <img
-            className="mx-auto"
-            src="https://i.ibb.co/K07jKtz/icon-4-png.webp"
-            alt=""
-            srcset=""
-          />
-          <p className="text-2xl font-serif text-black">Transportation</p>
-        </div>
-        <div>
-          <img
-            className="mx-auto"
-            src="https://i.ibb.co/K07jKtz/icon-4-png.webp"
-            alt=""
-            srcset=""
-          />
-          <p className="text-2xl font-serif text-black">Transportation</p>
-        </div>
-        <div>
-          <img
-            className="mx-auto"
-            src="https://i.ibb.co/K07jKtz/icon-4-png.webp"
-            alt=""
-            srcset=""
-          />
-          <p className="text-xl mt-2 font-serif text-black">Transportation</p>
-        </div>
-        <div>
-          <img
-            className="mx-auto"
-            src="https://i.ibb.co/K07jKtz/icon-4-png.webp"
-            alt=""
-            srcset=""
-          />
-          <p className="text-2xl font-serif text-black">Transportation</p>
-        </div>
+    <div className="grid mt-20 mb-10 gap-5 lg:grid-cols-4 md:grid-cols-4">
+      <div className="bg-white rounded-md py-7 px-10">
+        <img
+          className="mx-auto"
+          src="https://i.ibb.co/8cyqqmk/icon-1-png.webp "
+          alt=""
+          srcset=""
+        />
+        <p className=" hover:text-[#37ebd3] cursor-pointer text-xl text-center mt-4 font-serif text-black">
+          Transportation
+        </p>
       </div>
-      <div>
-        {/* https://i.ibb.co/K07jKtz/icon-4-png.webp
-            https://i.ibb.co/HKXTSwS/icon-3-png.webp
-            https://i.ibb.co/KmYgjNp/icon-2-png.webp
-            https://i.ibb.co/8cyqqmk/icon-1-png.webp */}
+      <div className="bg-white text-xl rounded-md py-7 px-10">
+        <img
+          className="mx-auto"
+          src="https://i.ibb.co/KmYgjNp/icon-2-png.webp"
+          alt=""
+          srcset=""
+        />
+        <p className="hover:text-[#37ebd3] cursor-pointer text-center text-xl mt-4 font-serif text-black">
+          Reiseservice
+        </p>
+      </div>
+      <div className="bg-white rounded-md py-7 px-10">
+        <img
+          className="mx-auto"
+          src="https://i.ibb.co/HKXTSwS/icon-3-png.webp"
+          alt=""
+          srcset=""
+        />
+        <p className="hover:text-[#37ebd3] cursor-pointer text-center text-xl mt-4 font-serif text-black">
+          Restaurant
+        </p>
+      </div>
+      <div className="bg-white rounded-md py-7 px-10">
+        <img
+          className="mx-auto"
+          src="https://i.ibb.co/K07jKtz/icon-4-png.webp"
+          alt=""
+          srcset=""
+        />
+        <p className="hover:text-[#37ebd3] cursor-pointer text-center text-xl mt-4 font-serif text-black">
+          Bar & Drink
+        </p>
       </div>
     </div>
   );
