22ee163ac89830ae37e6515f807e36db559e3b46
rehadhasan <rehadhasan664@gmail.com>
2023-07-12 12:00:24 +0600

    create About section for home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -6,13 +6,17 @@ import HomeBlog from "./HomeBlog/HomeBlog";
 import Offer from "./Offer/Offer";
 import HomeContact from "./HomeContact/HomeContact";
 import ServicesIcon from "./ServicesIcon/ServicesIcon";
+import About from "./About/About";
 
 const Home = () => {
   return (
     <div>
       <div>home</div>
       <div className="lg:mx-20 md:mx-12 mx-10">
-        {/* all all hotels */}
+        <About></About>
+        <p>Our Services</p>
+
+        {/* all hotels */}
         <CardCarousel></CardCarousel>
         <Offer></Offer>
         <ServicesIcon></ServicesIcon>
