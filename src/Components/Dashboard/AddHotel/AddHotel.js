ba870c9a41aa011c9def6c85f3049bfb31d6c1c2
rehadhasan <rehadhasan664@gmail.com>
2025-05-26 00:57:26 +0600

    update hotel image function

--- src/Components/Dashboard/AddHotel/AddHotel.js
+++ src/Components/Dashboard/AddHotel/AddHotel.js
@@ -16,7 +16,6 @@ const AddHotel = () => {
   } = useForm();
   const navigate = useNavigate();
   const imageHostKey = process.env.REACT_APP_imgbb_key;
-  // console.log(imageHostKey);
 
   const handleAddBlog = (data) => {
     const image = data.image[0];
